26807cb7ed12302153dd73e22a7fde96142f782f
Riro Maclaurine Chacha <riro.chacha@student.moringaschool.com>
2023-04-09 14:56:08 +0300

    completeda assignment

--- index.js
+++ index.js
@@ -1,1 +1,7 @@
-// Write your code here!
+const element = document.getElementById("main").remove()
+const newheader = document.createElement('hi')
+newheader.setAttribute('hi,vicory');
+newheader.innerHTML="YOUR-NAME is the champion";
+document.body.appendChild(newheader);
+
+
